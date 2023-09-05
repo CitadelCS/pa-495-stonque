a44662deac00e722fe1356317de73c8aa955dccc
mverdicchio <verdicchio@gmail.com>
2023-09-04 20:36:37 -0400

    two test classes, reformat

--- src/test/java/edu/citadel/AppTest.java
+++ src/test/java/edu/citadel/AppTest.java
@@ -7,14 +7,19 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
+class AnotherTestClass {
+    @Test
+    public void test() {
+        assertTrue(true);
     }
 }
